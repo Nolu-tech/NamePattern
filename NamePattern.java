1c0d9d6f9b0aca02019ebd0f97c89988fa57c6cf
Nolu-tech <noluthandomasekop@gmail.com>
2026-04-23 14:53:50 +0200

    Add files via upload

---
+++ NamePattern.java
@@ -0,0 +1,9 @@
+public class NamePattern {
+    public static void main(String[] args) {
+        
+        System.out.println("*   * ***** *     *   * *****  *   *   *   *   * ****  *****") ; 
+        System.out.println("* * * *   * *     *   *   *    *   * *   * * * * *   * *   *") ;
+        System.out.println("*  ** *   * *     *   *   *    ***** ***** *  ** *   * *   *") ;
+        System.out.println("*   * *   * *     *   *   *    *   * *   * *   * *   * *   *") ;
+        System.out.println("*   * ***** ***** *****   *    *   * *   * *   * ****  *****") ; }
+}
